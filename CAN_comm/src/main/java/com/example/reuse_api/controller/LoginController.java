5261ca0eb328b16721aec881ecce956405dcc3b5
HG-Shin <sinhuigwan7@naver.com>
2023-12-09 21:30:40 +0900

    user.json 아이디 업데이트

--- CAN_comm/src/main/java/com/example/reuse_api/controller/LoginController.java
+++ CAN_comm/src/main/java/com/example/reuse_api/controller/LoginController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -19,6 +26,22 @@ public class LoginController {
     @PostMapping("/user-id")
     public ResponseEntity<String> saveUserId(@RequestBody Map<String, String> user) {
         String userId = user.get("userid");
+
+        // Jackson ObjectMapper를 사용하여 JSON 파일에 쓰기
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> userData = new HashMap<>();
+        userData.put("userid", userId);
+
+        // Map을 List에 넣기
+        List<Map<String, String>> userDataList = new ArrayList<>();
+        userDataList.add(userData);
+
+        try {
+            // user.json 파일 경로를 적절하게 수정해주세요.
+            objectMapper.writeValue(new File("src/main/resources/user.json"), userDataList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return ResponseEntity.ok("User ID received: " + userId);
     }
