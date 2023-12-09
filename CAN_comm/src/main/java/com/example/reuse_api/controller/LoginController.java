3a3f41a47ead5b4555a93ebcabe0abb1a1e04b88
HG-Shin <sinhuigwan7@naver.com>
2023-12-09 20:58:38 +0900

    json 테스트

--- CAN_comm/src/main/java/com/example/reuse_api/controller/LoginController.java
+++ CAN_comm/src/main/java/com/example/reuse_api/controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.example.reuse_api.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -9,5 +14,12 @@ public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @PostMapping("/user-id")
+    public ResponseEntity<String> saveUserId(@RequestBody Map<String, String> user) {
+        String userId = user.get("userid");
+
+        return ResponseEntity.ok("User ID received: " + userId);
     }
 }
