28008f4ac18aff0111cc1eb0f46a9521901bb503
jihyung2 <wlgud1116@naver.com>
2023-12-12 05:16:34 +0900

    최종본, 웹 소켓 뺌 프론트코드가 부트스트랩과 연결되어있어서 실시간 웹 소켓을 연결하기엔 어려움 ( 다 뜯어 고쳐야함 )

--- CAN_comm/src/main/java/com/example/reuse_api/controller/restapi.java
+++ CAN_comm/src/main/java/com/example/reuse_api/controller/restapi.java
@@ -1,28 +1,21 @@
 package com.example.reuse_api.controller;
 
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-//import com.example.reuse_api.component.MyDatabaseComponent;
+
 import com.example.reuse_api.entity.*;
 import com.example.reuse_api.service.AllService;
 import com.example.reuse_api.service.ImageService;
 import com.example.reuse_api.service.VoiceService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.SQLException;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 // 가정, 위성에서 보낸 데이터 $(시작) %(종료) 기호로 구분하기
 // Map은 key와 value의 쌍으로 이루어진 데이터의 집합이다.
@@ -39,9 +32,6 @@ public class restapi {
 
     @Autowired
     private VoiceService voiceService;
-
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate; // SimpMessagingTemplate 주입
 
 
     private Map<String, String> sensorTypeMap;
@@ -66,6 +56,7 @@ public class restapi {
 
     @PostMapping("/sensor")
     public String sensorData(@RequestBody getsetdata data) throws IOException {
+
         String satelliteId = data.getSatname();
         String sensorid = data.getSensorname();
         String streamData = data.getData();
@@ -83,7 +74,7 @@ public class restapi {
             imageData.setUserid(satelliteId);
             imageData.setData(imageBytes);
             imageService.saveimageDB(imageData);
-            messagingTemplate.convertAndSend("/topic/image-updates", imageData);
+            //messagingTemplate.convertAndSend("/topic/image-updates", imageData);
 
         }
         else if (sensorName.equals("Voice")){
@@ -92,14 +83,16 @@ public class restapi {
             voiceData.setUserid(satelliteId);
             voiceData.setData(voiceBytes);
             voiceService.savevoiceDB(voiceData);
-            messagingTemplate.convertAndSend("/topic/db-updates", voiceData);
+            //messagingTemplate.convertAndSend("/topic/db-updates", voiceData);
         }
         else { // 그외 나머지 센서
             allStoreData.setName(sensorName);
             allStoreData.setUserid(satelliteId);
             allStoreData.setData(streamData);
             allService.saveAllDB(allStoreData);
-            messagingTemplate.convertAndSend("/topic/table-updates", allStoreData);
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            String allStoreDataJson = objectMapper.writeValueAsString(allStoreData);
+//            webSocketHandler.sendTextMessageToAll("77777New " + sensorName + " data received from " + satelliteId);
         }
         return "Sensor data processed successfully";
     }
